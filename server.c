b975d20ab1c787dff8683c14f8db2eee019ef2b8
emejqz <emejqz@gmail.com>
2017-09-07 11:44:43 +0000

    one-way one-time mssging

--- server.c
+++ server.c
@@ -7,19 +7,24 @@
 
 int main(int argc, char const *argv[])
 {
-  int server_fd, new_socket, valread;
+  int server_fd, new_socket;//, valread;
   struct sockaddr_in address;
-  int opt = 1;
+  //  int opt = 1;
+  int len = 0;
   int addrlen = sizeof(address);
   char buffer[1024] = {0};
   //char *hello = "Hello from server";
       
+  printf("go");
+
   // Creating socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
       perror("socket failed");
       exit(EXIT_FAILURE);
     }
+
+  printf("socketed");
     
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
@@ -32,24 +37,36 @@ int main(int argc, char const *argv[])
       perror("bind failed");
       exit(EXIT_FAILURE);
     }
+
+  printf("binded");
+
   if (listen(server_fd, 5) < 0)
     {
       perror("listen");
       exit(EXIT_FAILURE);
     }
-  while(1){
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-			     (socklen_t*)&addrlen))<0)
-      {
-	perror("accept");
-	exit(EXIT_FAILURE);
-      }
-    while(len=recv(new_socket, buffer, sizeof(buffer), 0)){
-      //TODO: read and print
-      printf("%s", buffer)
-	}
-    close(new_socket);
-  }
+
+  printf("listened");
+
+  //while(1){
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+			   (socklen_t*)&addrlen))<0)
+    {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+ 
+  //while( 
+  recv(new_socket, buffer, sizeof(buffer), 0); //){
+    
+  printf("%s", buffer);  
+      
+    
+    
+  
+  
+  close(new_socket);
+  //}
   return 0;
 }
 
