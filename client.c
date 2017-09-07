effa51eae64ac3d01bef85aec735b6b918ae440f
songzhe <songzhezhu@email.arizona.edu>
2017-09-07 03:21:07 -0700

    new

--- client.c
+++ client.c
@@ -1,15 +1,15 @@
-//Description: Client side of the Moblie TCP Proxy project
-//Author: Eric Evans, Songzhe Zhu
-//Date: Sep 7 2017
 #include <stdio.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <netinet/in.h>
+#include <sys/types.h>
 #include <string.h>
 
 int main(int argc, char const *argv[])
 {
-        struct sockaddr_in address;
+        //struct sockaddr_in address;
         int sock = 0, valread;
         struct sockaddr_in serv_addr;
         char buffer[1024] = {0};
@@ -24,14 +24,15 @@ int main(int argc, char const *argv[])
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_port = htons(atoi(argv[2]));
         serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-  
+
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         {
                 printf("\nConnection Failed \n");
                 return -1;
         }
-        send(sock , hello , strlen(hello) , 0 );
-        close(sock);
+        send(sock , "hello" , strlen("hello") , 0 );
+        printf("check");
+        close(sock); 
         return 0;
 }
 
