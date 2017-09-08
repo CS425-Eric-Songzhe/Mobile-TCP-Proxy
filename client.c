dcc454df8b9050fd01c88b5034667179c7a3e442
Songzhe Zhu <songzhezhu@cs.arizona.edu>
2017-09-07 18:33:56 -0700

    server and client successfully talk to each other

--- client.c
+++ client.c
@@ -1,3 +1,6 @@
+//Description: Client side of the Moblie TCP Proxy project
+//Author: Eric Evans, Songzhe Zhu
+//Date: Sep 7 2017
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -6,33 +9,32 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <string.h>
-
+  
 int main(int argc, char const *argv[])
 {
-  //struct sockaddr_in address;
-  int sock = 0;//, valread;
-  struct sockaddr_in serv_addr;
-  //  char buffer[1024] = {0};
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-      printf("\n Socket creation error \n");
-      return -1;
-    }
-
-  memset(&serv_addr, '0', sizeof(serv_addr));
-
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(atoi(argv[2]));
-  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-      printf("\nConnection Failed \n");
-      return -1;
-    }
-  send(sock , "hello" , strlen("hello") , 0 );
-  printf("check");
-  close(sock); 
-  return 0;
+	//struct sockaddr_in address;
+    	int sock = 0; //valread;
+    	struct sockaddr_in serv_addr;
+    	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    	{
+        	printf("\n Socket creation error \n");
+        	return -1;
+    	}
+  
+    	memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    	serv_addr.sin_family = AF_INET;
+    	serv_addr.sin_port = htons(atoi(argv[2]));
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);      		
+  
+    	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    	{	
+        	printf("\nConnection Failed \n");
+        	return -1;
+    	}
+	printf("Successfully connected to server\n");
+    	send(sock , "hello\n" , strlen("hello") , 0 );
+	//printf("check");
+	//close(sock);
+    	return 0;
 }
-
--- server.c
+++ server.c
@@ -1,72 +1,71 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
+//Description: Server side of the Moblie TCP Proxy project
+//Author: Eric Evans, Songzhe Zhu
+//Date: Sep 7 2017
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
 #include <string.h>
-
+  
 int main(int argc, char const *argv[])
 {
-  int server_fd, new_socket;//, valread;
-  struct sockaddr_in address;
-  //  int opt = 1;
-  int len = 0;
-  int addrlen = sizeof(address);
-  char buffer[1024] = {0};
-  //char *hello = "Hello from server";
+	int server_fd, new_socket;
+    	struct sockaddr_in address;
+	int len = 0;
+    	int addrlen = sizeof(address);
+    	char buffer[1024] = {0};
+    //char *hello = "Hello from server";
       
-  printf("go");
-
-  // Creating socket file descriptor
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-      perror("socket failed");
-      exit(EXIT_FAILURE);
-    }
-
-  printf("socketed");
-    
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(atoi(argv[1]));
+    	// Creating socket file descriptor
+    	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    	{
+        	perror("socket failed");
+        	exit(EXIT_FAILURE);
+    	}
       
-  // Forcefully attaching socket to the port 8080
-  if(bind(server_fd, (struct sockaddr *)&address, 
-	  sizeof(address))<0)
-    {
-      perror("bind failed");
-      exit(EXIT_FAILURE);
-    }
-
-  printf("binded");
-
-  if (listen(server_fd, 5) < 0)
-    {
-      perror("listen");
-      exit(EXIT_FAILURE);
-    }
-
-  printf("listened");
-
-  //while(1){
-  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-			   (socklen_t*)&addrlen))<0)
-    {
-      perror("accept");
-      exit(EXIT_FAILURE);
-    }
- 
-  //while( 
-  recv(new_socket, buffer, sizeof(buffer), 0); //){
-    
-  printf("%s", buffer);  
+    	address.sin_family = AF_INET;
+    	address.sin_addr.s_addr = INADDR_ANY;
+    	address.sin_port = htons(atoi(argv[1]));
       
-    
-    
-  
-  
-  close(new_socket);
-  //}
-  return 0;
+    	// Forcefully attaching socket to the port 8080
+    	if(bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    	{
+        	perror("bind failed");
+        	exit(EXIT_FAILURE);
+    	}
+    	if (listen(server_fd, 5) < 0)
+    	{
+        	perror("listen");
+        	exit(EXIT_FAILURE);
+    	}
+	while(1){
+    		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    		{
+        		perror("accept");
+        		exit(EXIT_FAILURE);
+    		}
+		else{
+			printf("A client connected!\n");
+		}
+		//This is the size of the input, returns 5 for "hello"
+		//len = recv(new_socket, buffer, sizeof(buffer), 0);
+		//printf("%d", len);
+
+		while(len=recv(new_socket, buffer, sizeof(buffer), 0)/*len*/>0){
+			//TODO: read and print
+			printf("Client: %s\n", buffer);
+		}
+		if(len==0){
+			printf("Nothing more from the client. Client has been shut down.\n");
+		}
+		if(len<0){
+			printf("ERROR on RECV()!\n");
+		}
+		//printf("%s", buffer);
+		//close(new_socket);
+    	}
+    	return 0;
 }
-
