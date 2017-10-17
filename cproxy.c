96136af3a7060c3c3ab0b21b2d68213a2464a6ab
Songzhe Zhu <songzhezhu@email.arizona.edu>
2017-10-17 19:50:59 +0000

    cp: ack and rename s's

--- cproxy.c
+++ cproxy.c
@@ -21,7 +21,7 @@
  */
 int main(int argc, char const *argv[])
 {
-    int s1 = 0, s2 = 0, n = 0, rv = 0;
+    int s1_telnet = 0, s2_sproxy = 0, n = 0, rv = 0;
     fd_set readfds;
     struct timeval tv;
     char cmd_buf[9999], reply_buf[9999];
@@ -55,7 +55,7 @@ int main(int argc, char const *argv[])
 
     // Accept telnet
     //printf("Accepting request from telnet\n");
-    s1 = accept_client(sock_telnet, &addr_telnet);
+    s1_telnet = accept_client(sock_telnet, &addr_telnet);
     //printf("- telnet request accepted\n");
     int sessionID = rand();
 
@@ -84,7 +84,7 @@ int main(int argc, char const *argv[])
     	printf("- connected to client\n");
 
 	// sproxy
-	s2 = sock;
+	s2_sproxy = sock;
 
 	// Set up heartbeat time interval checking
         struct timeval last, now, hb_time;
@@ -93,18 +93,19 @@ int main(int argc, char const *argv[])
 	int hb_recv = 0;
 	int last_hb = -1;
 	while (1) {
+	  printf("---------------------------------------\n");
 	    // clear the set ahead of time
 	    FD_ZERO(&readfds);
 
 	    // add our descriptors to the set
-	    FD_SET(s1, &readfds);
-	    FD_SET(s2, &readfds);
+	    FD_SET(s1_telnet, &readfds);
+	    FD_SET(s2_sproxy, &readfds);
 
 	    // find the largest descriptor, and plus one.
-	    if (s1 > s2)
-		n = s1 + 1;
+	    if (s1_telnet > s2_sproxy)
+		n = s1_telnet + 1;
 	    else
-		n = s2 + 1;
+		n = s2_sproxy + 1;
 
 
 	    // wait until either socket has data ready to be recv()d (timeout 1.5 secs)
@@ -124,7 +125,7 @@ int main(int argc, char const *argv[])
                 int msg_len_HB =
 		  make_msg(msg_HB, HEARTBEAT, hb_sent, sessionID,
 			   sizeof(payload_HB), payload_HB);
-                send(s2, msg_HB, msg_len_HB, 0);
+                send(s2_sproxy, msg_HB, msg_len_HB, 0);
                 printf("print HB %d\n", hb_sent);
                 gettimeofday(&last, NULL);
                 hb_sent++;
@@ -138,7 +139,7 @@ int main(int argc, char const *argv[])
                         ((now.tv_usec - hb_time.tv_usec) / 1000000.0);
                 if (diff_hb >= 3 /*&& sessionID > 0*/) {
                         printf("HB Timeout occured\n");
-			//close(s1);
+			//close(s1_telnet);
 			printf("Socket to sproxy closed.\n");
 			break;
                 }
@@ -151,18 +152,28 @@ int main(int argc, char const *argv[])
 		;		//printf("Timeout occurred!  No data after 10.5 seconds.\n");
 	    } else {
 		// one or both of the descriptors have data
-		// s1: Telnet
-		if (FD_ISSET(s1, &readfds)) {
-		    len1 = recv(s1, cmd_buf, sizeof(cmd_buf), 0);
+		// s1_telnet: Telnet
+		if (FD_ISSET(s1_telnet, &readfds)) {
+		    len1 = recv(s1_telnet, cmd_buf, sizeof(cmd_buf), 0);
 		    //printf("Recved command from telnet: %s, \n %d\n", cmd_buf, len1);
+
+		    // Send Acknowledgment                                     
+                    char msg_ack[9999] = { 0 };
+                    char *ack_str = "ack";
+                    int msg_len_ack =
+                      make_msg(msg_ack, ACK, 0, sessionID, strlen(ack_str), ack_str);
+                    send(s2_sproxy, msg_ack, msg_len_ack, 0);
+		    printf("sent acknow\n");
+
+		    // make message
 	            char msg_t[9999] = {0};
 		    int msg_len_t = make_msg(msg_t, DATA, 0, sessionID, len1, cmd_buf);
-		    send(s2, msg_t, msg_len_t, 0);
+		    send(s2_sproxy, msg_t, msg_len_t, 0);
 		    memset(cmd_buf, 0, sizeof(cmd_buf));
 		}
-		// s2: Sproxy
-		if (FD_ISSET(s2, &readfds)) {
-		    recv(s2, reply_buf, sizeof(reply_buf), 0);
+		// s2_sproxy: Sproxy
+		if (FD_ISSET(s2_sproxy, &readfds)) {
+		    recv(s2_sproxy, reply_buf, sizeof(reply_buf), 0);
 		    
 		    int type = -1;
 		    int ackID = -1;
@@ -188,8 +199,12 @@ int main(int argc, char const *argv[])
 		    else if (type == DATA) {
 			printf("Recieved Data from %d\n", sessionID_S);
 			printf("Recved command from sproxy: |%.*s|\n, %d\n", (int)sizeof(reply_buf), reply_buf, paylen_s);
-			send(s1, payload_s, paylen_s, 0);
-		    } else {
+			send(s1_telnet, payload_s, paylen_s, 0);
+		    } 
+		    else if (type == ACK){
+		      printf("Recvd Acknowledgement from %d, %d\n", sessionID_S, ackID);
+		    }
+		    else {
 			printf("ERROR: unknown message type: %d\n", type);
 		    }
 
@@ -198,12 +213,12 @@ int main(int argc, char const *argv[])
 	    }
 
 	}
-	close(s1);
-	close(sock);
-	//close(s2);
+	close(s2_sproxy);
+	//close(sock);
+	//close(s2_sproxy);
     }
 
-    close(s2);
+    close(s1_telnet);
     close(sock_telnet);
     return 0;
 }
