eec8576f8f69b92630eb741c3829f4235b1d877a
Songzhe Zhu <songzhezhu@email.arizona.edu>
2017-10-19 19:52:25 +0000

    cp: remove prints

--- cproxy.c
+++ cproxy.c
@@ -23,12 +23,6 @@ void run(char const *ip, int port_sproxy, int port_telnet);
  */
 int main(int argc, char const *argv[])
 {
-  /*
-    int s1_telnet = 0, s2_sproxy = 0, n = 0, rv = 0;
-    fd_set readfds;
-    struct timeval tv;
-    char cmd_buf[9999], reply_buf[9999];
-  */
     // Read Arguments
     char const *ip = argv[2];	// server ip address
     int port_sproxy = atoi(argv[3]);	// server port
@@ -53,17 +47,6 @@ void run(char const *ip, int port_sproxy, int port_telnet){
     struct timeval tv;
     char cmd_buf[9999], reply_buf[9999];
 
-    // Setup Sockets
-    //printf("Setting up socket for sproxy\n");
-    //struct sockaddr_in serv_addr;
-    //int sock = setup_socket(&serv_addr, ip, port_sproxy);
-    //  printf("- socket for sproxy open\n");
-
-    //printf("Listening for client connect request\n");
-    //connect_to_server(&serv_addr, sock);
-    //int sessionID = rand();
-    //printf("- connected to client\n");
-
     //printf("Setting up socket for telnet\n");
     struct sockaddr_in addr_telnet;
     int sock_telnet = setup_socket(&addr_telnet, "127.0.0.1", port_telnet);
@@ -72,13 +55,11 @@ void run(char const *ip, int port_sproxy, int port_telnet){
     // Connect with Telnet
     //printf("Listening for telnet connect request\n");
     bind_and_listen(sock_telnet, &addr_telnet, 5);
-    //connect_to_server(&addr_telnet, sock_telnet);
     //printf("- connected to telnet\n");
 
     // Accept telnet
     //printf("Accepting request from telnet\n");
     s1_telnet = accept_client(sock_telnet, &addr_telnet);
-    //printf("- telnet request accepted\n");
     //printf("- telnet request accepted\n");
     int sessionID = rand();
 
@@ -90,7 +71,6 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 	printf("Setting up socket for sproxy\n");
 	struct sockaddr_in serv_addr;
 	int sock = setup_socket(&serv_addr, ip, port_sproxy);
-	printf("- socket:%d  for sproxy open\n", sock);
 
 	// Forcefully attaching socket to the port 8080
 	int opt = 1;
@@ -149,7 +129,7 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 			     sizeof(payload_HB), payload_HB);
 		usleep(500);
 		send(s2_sproxy, msg_HB, msg_len_HB, 0);
-		printf("print HB %d\n", hb_sent);
+		printf("send HB %d\n", hb_sent);
 		gettimeofday(&last, NULL);
 		hb_sent++;
 	    }
@@ -159,12 +139,10 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 		double diff_hb =
 		    (now.tv_sec - hb_time.tv_sec) +
 		    ((now.tv_usec - hb_time.tv_usec) / 1000000.0);
-		if (diff_hb >= 3 /*&& sessionID > 0 */ ) {
+		if (diff_hb >= 3 ) {
 		    printf("HB Timeout occured\n");
 		    close(s2_sproxy);
-		    printf
-			("Socket to sproxy closed. Reconnecting a new one -------------------\n");
-		    //break;
+		   
 		    struct sockaddr_in new_addr;
 		    s2_sproxy = setup_socket(&new_addr, ip, port_sproxy);
 
@@ -172,10 +150,9 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 		    do {
 			printf("Attempting to reconnect...\n");
 			try = connect_to_server(&new_addr, s2_sproxy);
-			//usleep(5000);
 		    } while (try == 0);
+		    //avoid reconnection unti recv hb
 		    gettimeofday(&hb_time, NULL);
-		    //diff_hb = 0; //avoid reconnection unti recv hb
 		}
 	    }
 
@@ -183,22 +160,15 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 	    if (rv == -1) {
 		perror("select");	// error occurred in select()
 	    } else if (rv == 0) {
-		;		//printf("Timeout occurred!  No data after 10.5 seconds.\n");
+		;	  
 	    } else {
 		// one or both of the descriptors have data
 		// s1_telnet: Telnet
 		if (FD_ISSET(s1_telnet, &readfds)) {
 		    len1 = recv(s1_telnet, cmd_buf, sizeof(cmd_buf), 0);
-		    if (len1 > 0) {
-			printf("Recvd from s1_telnet: %d\n", len1);
-			int i = 0;
-			for (i = 0; i < len1; i++) {
-			    printf("%c", cmd_buf[i]);
-			}
-			printf("\n");
+		    if (len1 > 0) {\
 
-
-			// Send Acknowledgment                                     
+			// Send Acknowledgment                                 
 			char msg_ack[9999] = { 0 };
 			char *ack_str = "ack";
 			int msg_len_ack =
@@ -217,36 +187,10 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 			send(s2_sproxy, msg_t, msg_len_t, 0);
 			memset(cmd_buf, 0, sizeof(cmd_buf));
 		    } else { // len < 1
-			printf
-			    ("Telnet connect failed, try to reconnect.\n");
 			close(s1_telnet);
 			close(s2_sproxy);
 			close(sock_telnet);
 			return;
-			/*   struct sockaddr_in new_addr_telnet;
-			   int new_sock_telnet = setup_socket(&new_addr_telnet, "127.0.0.1", port_telnet);
-			   //printf("- socket for telnet open\n");
-
-			   // Connect with Telnet
-			   printf("Listening for telnet connect request\n");
-			   listen(new_sock_telnet, 5);
-			   //connect_to_server(&addr_telnet, sock_telnet);
-			   printf("- connected to telnet\n");
-
-			   // Accept telnet
-			   printf("Accepting request from telnet\n");
-			   s1_telnet = accept_client(new_sock_telnet, &new_addr_telnet);
-			   printf("Accepted with new telnet at sock: %d\n", s1_telnet);
-			   
-			   s2_sproxy = setup_socket(&serv_addr, ip, port_sproxy);
-			   printf("- socket:%d  for sproxy open\n", s2_sproxy);
-
-			   printf("Listening for client connect request\n");
-			   connect_to_server(&serv_addr, s2_sproxy);
-			   printf("- connected to client\n");
-
-			   sessionID = rand();
-			   continue; */
 		    }
 		}
 		// s2_sproxy: Sproxy
@@ -254,12 +198,6 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 		    len2 =
 			recv(s2_sproxy, reply_buf, sizeof(reply_buf), 0);
 		    if (len2 > 0) {
-			printf("Recvd from s2_cproxy: %d\n", len2);
-			int i = 0;
-			for (i = 0; i < len2; i++) {
-			    printf("%c", reply_buf[i]);
-			}
-			printf("\n");
 
 			char *pkt_buf = reply_buf;	// for shifting   
 			int pkt_len = len2;	// keeping track of where to start parsing
@@ -282,25 +220,16 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 				gettimeofday(&hb_time, NULL);
 				hb_recv++;
 				last_hb++;
-				//if(ackID == last_hb){
-				//      exit(0);
-				//}
-				//last_hb = ackID;
 			    }
 			    // else, if message is data, send payload
 			    else if (type == DATA) {
 				printf("Recieved Data from %d\n",
 				       sessionID_S);
-				printf
-				    ("Recved command from sproxy: |%.*s|\n, %d\n",
-				     (int) sizeof(reply_buf), reply_buf,
-				     paylen_s);
-				usleep(500);
+				//printf("Recved command from sproxy: |%.*s|\n, %d\n",(int) sizeof(reply_buf), reply_buf, paylen_s);
+				//usleep(500);
 				send(s1_telnet, payload_s, paylen_s, 0);
 			    } else if (type == ACK) {
-				printf
-				    ("Recvd Acknowledgement from %d, %d\n",
-				     sessionID_S, ackID);
+			      ;//printf("Recvd Acknowledgement from %d, %d\n", sessionID_S, ackID);
 			    } else {
 				printf("ERROR: unknown message type: %d\n",
 				       type);
@@ -312,10 +241,8 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 				go_again = 1;
 				// truncate pkt_len      
 				pkt_len = pkt_len - hdr_and_pay;
-				// shift beginning of cmd_buf to next message    
+				// shift beginning of cmd_buf to next message  
 				pkt_buf += hdr_and_pay;
-				printf
-				    ("~~WOW multiple messages! going to go again.~~\n");
 			    } else {
 				go_again = 0;
 			    }
@@ -325,8 +252,6 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 
 			memset(reply_buf, 0, sizeof(reply_buf));
 		    } else { // len < 1
-			printf
-			    ("(LEN < 1) sproxy connection fails, trying to reconnect ---------------------\n");
 			close(s2_sproxy);
 			s2_sproxy =
 			    setup_socket(&serv_addr, ip, port_sproxy);
@@ -340,12 +265,10 @@ void run(char const *ip, int port_sproxy, int port_telnet){
 	    }
 
 	}
-	//close(s2_sproxy);
 	close(sock);
 	close(s2_sproxy);
     }
-    //close(s2_sproxy);
     close(s1_telnet);
     close(sock_telnet);
-    return;// 0;
+    return;
 }
